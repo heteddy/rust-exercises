d7e05fdfd6ddacbaa8272d1ae495025dbccb8e9a
heteddy <heteddy@126.com>
2024-06-08 23:45:57 +0800

    验证bert dao

--- s1/actionkv/src/endpoint/bert.rs
+++ s1/actionkv/src/endpoint/bert.rs
@@ -2,7 +2,7 @@ use crate::cache::repo;
 use crate::cache::sync;
 use crate::dao::bert::BertEntity;
 use crate::middleware::auth::auth_middleware;
-use crate::pb::svr::{bert::BertReq, bert::BertResp, ApiError, ApiResponse};
+use crate::pb::svr::{bert::BertReq, bert::BertResp, ApiError, ApiResponse, Pagination};
 use crate::server;
 use axum::extract::{Json, Path, Query, State};
 use axum::handler::Handler;
@@ -53,6 +53,19 @@ async fn update(
     let e = svc.update(id, b).await?;
     Ok(ApiResponse::from_result(e.into()))
 }
+#[instrument(skip_all)]
+async fn list(
+    State(svc): State<server::bert::BertSvc>,
+    Query(pag): Query<Pagination>,
+) -> Result<ApiResponse<Vec<BertResp>>, ApiError> {
+    let s = span!(Level::INFO, "list");
+    let _enter = s.enter();
+
+    // let b = BertEntity::from(payload);
+    let entities = svc.list(pag.skip, pag.limit).await?;
+    let resp = entities.into_iter().map(|e| e.into()).collect();
+    Ok(ApiResponse::from_result(resp))
+}
 
 #[instrument(skip_all)]
 async fn del(
@@ -69,8 +82,8 @@ async fn del(
 pub fn register_route(tx: mpsc::Sender<sync::SyncData>) -> Router {
     let svc = server::bert::BertSvc::new(tx);
     let mut _route = Router::new();
-    // let middle_svc = server::auth::TENANT_AUTH_SVC.clone();
-    let middle_svc = repo::IndexConfigRepo::get_instance();
+
+    // let middle_svc = repo::IndexConfigRepo::get_instance();
 
     // todo 新构建一个route然后使用route_layer 添加middleware
     // _route = _route.route(
@@ -80,7 +93,7 @@ pub fn register_route(tx: mpsc::Sender<sync::SyncData>) -> Router {
     // );
     // https://docs.rs/axum/latest/axum/middleware/index.html#passing-state-from-middleware-to-handlers
     //.route_layer(from_fn_with_state(middle_svc, auth_middleware));
-    
+    _route = _route.route("/berts", get(list).post(create));
     _route = _route.route("/berts/:id", get(retrieve).put(update).delete(del));
     Router::new().nest("/api", _route).with_state(svc)
 }
